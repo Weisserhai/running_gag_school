5f9e63e803207933df8f6c52dac9df7d27f8648b
Ilyas Boran <ilyas.brn@gmail.com>
2023-10-27 12:25:25 +0200

    fix create_database.sql

--- digitale_hausverwaltung/src/main/resources/sql/create_database.sql
+++ digitale_hausverwaltung/src/main/resources/sql/create_database.sql
@@ -4,5 +4,5 @@ DROP DATABASE IF EXISTS db;
 CREATE DATABASE db CHARACTER SET utf8;
 
 CREATE USER 'running_gag'@'localhost';
-GRANT ALL PRIVILEGES ON deine_datenbank.* TO 'dein_benutzername'@'localhost';
+GRANT ALL PRIVILEGES ON db.* TO 'running_gag'@'localhost';
 FLUSH PRIVILEGES;
