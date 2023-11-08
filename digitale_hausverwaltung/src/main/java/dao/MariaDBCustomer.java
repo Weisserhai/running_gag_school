d5e1cf37b86fe45483badabb9b214c02d8c9b582
MaxPower <max.kiehn@outlook.de>
2023-11-08 08:13:19 +0100

    Finish get and set for Customer (not tested yet)

--- digitale_hausverwaltung/src/main/java/dao/MariaDBCustomer.java
+++ digitale_hausverwaltung/src/main/java/dao/MariaDBCustomer.java
@@ -4,12 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import models.Customer;
-import dao.MariaDBFacManDAO;
-
 public class MariaDBCustomer implements CustomerDAO {
     private Connection connection = null;
 
@@ -24,27 +23,29 @@ public class MariaDBCustomer implements CustomerDAO {
             ps.setObject(1, UUID.randomUUID());
             ps.setString(2, firstname);
             ps.setString(3, lastname);
-            ResultSet resultSet = ps.executeQuery();
-            Customer customer = new Customer(
-                id,
-                resultSet.getString("firstName"),
-                resultSet.getString("lastName")
-            );
-            while(resultSet.next()){
-                return customer;
-            }
+            int resultSet = ps.executeUpdate();
+            return resultSet;
 
         } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error from create without Object: " + e.getMessage());
             e.printStackTrace();
         }
-        return null;
+        return 0;
     }
 
     @Override
     public int create(Customer customer) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+        try {
+            PreparedStatement ps = connection.prepareStatement("Insert into USERS values ?");
+            ps.setObject(1, customer);
+            int resultSet = ps.executeUpdate();
+            return resultSet;
+
+        } catch (SQLException e) {
+            System.out.println("Error from create with Object: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     @Override
@@ -63,7 +64,7 @@ public class MariaDBCustomer implements CustomerDAO {
             }
 
         } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error from get: " + e.getMessage());
             e.printStackTrace();
         }
         return null;
@@ -71,26 +72,73 @@ public class MariaDBCustomer implements CustomerDAO {
 
     @Override
     public List<Customer> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        try {
+            PreparedStatement ps = connection.prepareStatement("Select * from Customer");
+            ResultSet resultSet = ps.executeQuery();
+            List<Customer> list = new ArrayList<Customer>();
+
+            while (resultSet.next()){
+                Customer customer = new Customer(
+                (UUID) resultSet.getObject("ID"),
+                resultSet.getString("firstName"),
+                resultSet.getString("lastName")
+            );
+                list.add(customer);
+            }
+            return list;
+        } catch (SQLException e) {
+            System.out.println("Error from getAll: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
     public boolean update(int id, String firstname, String lastname) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        try {
+            PreparedStatement ps = connection.prepareStatement("Update Customer set firstName = ?, firstName = ? where ID = ?");
+            ps.setString(1, firstname);
+            ps.setString(2,lastname);
+            ps.setObject(3, id);
+            int resultSet = ps.executeUpdate();
+            return (resultSet != 0);
+
+
+        } catch (SQLException e) {
+            System.out.println("Error from update without Object: " + e.getMessage());
+            e.getStackTrace();
+        }
+        return false;
     }
 
     @Override
     public boolean update(Customer customer) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        try {
+            PreparedStatement ps = connection.prepareStatement("Update Customer set firstName = ?, firstName = ? where ID = ?");
+            ps.setString(1,customer.getFirstname());
+            ps.setString(2, customer.getLastname());
+            ps.setObject(3, customer.getId());
+            int resultSet = ps.executeUpdate();
+            return (resultSet != 0);
+        } catch (SQLException e) {
+            System.out.println("Error from update with Object: " + e.getMessage());
+            e.getStackTrace();
+        }
+        return false;
     }
 
     @Override
     public boolean delete(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        try {
+            PreparedStatement ps = connection.prepareStatement("Delete from Customer where ID = ?");
+            ps.setObject(1, id);
+            int resultSet = ps.executeUpdate();
+            return (resultSet != 0);
+        } catch (SQLException e) {
+            System.out.println("Error from delete: " + e.getMessage());
+            e.getStackTrace();
+        }
+        return false;
     }
 
 }
