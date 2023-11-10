cc320935ff575f5eb931f492ca085c4fefd1b49c
MaxPower <max.kiehn@outlook.de>
2023-11-10 12:11:17 +0100

    fake merge

--- digitale_hausverwaltung/src/main/java/dao/FacManDAO.java
+++ digitale_hausverwaltung/src/main/java/dao/FacManDAO.java
@@ -1,21 +1,15 @@
 package dao;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-
-
-
 public interface FacManDAO {
 
 
-	void createDatabaseFacMan();
+	int createDatabaseFacMan();
 
 
-	void createTableCustomer();
+	int createTableCustomer();
 
 
-	void createTableReading();
+	int createTableReading();
 
 
 
--- digitale_hausverwaltung/src/main/java/dao/MariaDBFacManDAO.java
+++ digitale_hausverwaltung/src/main/java/dao/MariaDBFacManDAO.java
@@ -7,23 +7,115 @@ import java.sql.SQLException;
 
 
 public class MariaDBFacManDAO implements FacManDAO{
+    // Main Methode ausführen
+    public static void main(String[] args) {
+        MariaDBFacManDAO mariaDBFacManDAO = new MariaDBFacManDAO();
+        mariaDBFacManDAO.createDatabaseFacMan();
+        mariaDBFacManDAO.createTableCustomer();
+        mariaDBFacManDAO.createTableReading();
+    }
+
+
     @Override
-    public void createDatabaseFacMan() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createDatabaseFacMan'");
+    public int createDatabaseFacMan() {
+        // Erstellung der Datenbank
+            String databaseName = "running_gag";
+            String jdbcUrl = "jdbc:mariadb://localhost:3307/mysql";
+            String user = "root";
+            String password = "root";
+
+            try {
+                Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
+
+                java.sql.Statement statement = connection.createStatement();
+                String createDatabaseSQL = "CREATE DATABASE " + databaseName + ";";
+                statement.execute(createDatabaseSQL);
+
+                System.out.println("Datenbank wurde erfolgreich erstellt.");
+
+                statement.close();
+                connection.close();
+                return 0;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return 1;
+            }
     }
 
     @Override
-    public void createTableCustomer() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createTableCustomer'");
+    public int createTableCustomer() {
+        // Erstellung der Customer Tabelle
+                String databaseName = "running_gag";
+                String tableName = "Customer";
+                String jdbcUrl = "jdbc:mariadb://localhost:3307/running_gag";
+                String user = "root";
+                String password = "root";
+
+                try {
+                    Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
+
+                    java.sql.Statement statement = connection.createStatement();
+
+                    String useDatabase = "Use " + databaseName + ";";
+
+                    statement.execute(useDatabase);
+                    String createTableSQL = "CREATE TABLE IF NOT EXISTS Customer ("
+                            + "ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL, "
+                            + "UUID VARCHAR(36) NOT NULL, "
+                            + "firstName VARCHAR(50) NOT NULL, "
+                            + "lastName VARCHAR(50) NOT NULL)";
+                    statement.execute(createTableSQL);
+
+                    System.out.println("Tabelle " + tableName + " wurde erfolgreich erstellt.");
+
+                    statement.close();
+                    connection.close();
+                    return 0;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return 1;
+                }
     }
 
     @Override
-    public void createTableReading() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createTableReading'");
+    public int createTableReading() {
+        // Erstellung der Reading Tabelle
+                String databaseName = "running_gag";
+                String tableName = "Reading";
+                String jdbcUrl = "jdbc:mariadb://localhost:3307/running_gag";
+                String user = "root";
+                String password = "root";
+
+                try {
+                    Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
+
+                    java.sql.Statement statement = connection.createStatement();
+
+                    String useDatabase = "Use " + databaseName + ";";
+                    statement.execute(useDatabase);
+
+                    String createTableSQL = "CREATE TABLE IF NOT EXISTS " + tableName + " ("
+                            + "ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL, "
+                            + "UUID VARCHAR(36) NOT NULL, "
+                            + "customerId int NOT NULL,"
+                            + "dateOfReading date NOT NULL, "
+                            + "typeOfReading varchar(50) NOT NULL, "
+                            + "meterCount int NOT NULL, "
+                            + "comment varchar(50) NOT NULL DEFAULT'', "
+                            + "FOREIGN KEY (customerId) REFERENCES Customer(ID))";
+                    statement.execute(createTableSQL);
+
+                    System.out.println("Tabelle " + tableName + " wurde erfolgreich erstellt.");
+
+                    statement.close();
+                    connection.close();
+                    return 0;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return 1;
+                }
     }
+
 
     public static Connection connectToMariaDB() {
         Connection connection = null;
